9a07235977a0add05481b072ad8be5839f61927c
TheRealAGBOOLA <agboolasamson602@gmail.com>
2023-03-21 14:21:54 +0100

    edited index.html
    added the start page and game play interface and also scripted them

--- script.js
+++ script.js
@@ -4,28 +4,35 @@ let userName = document.getElementById("user-name");
 let userEl = document.getElementById("usr-nm");
 let balVal = document.getElementById("balance");
 let errMsg = document.getElementById("errmsg");
+let gamePage = document.getElementById("game-page");
+let startGame = document.getElementById("start-game");
 let balLevel = 100;
-
 
 function proceed() {
     welCome.style.display = "none";
     logIn.style.display = "block"
-
 }
 
 function play() {
     if (userEl.value.length != 0) {
         userName.innerText = "Username:" + " " + userEl.value;
-        //console.log(userName.innerText);
         balVal.innerText = "Balance:" + " " + balLevel;
+        logIn.style.display = "none";
+        startGame.style.display = "block";
+        document.getElementById("gamebox").style.height = "200px auto";
     } else {
         errMsg.style.display = "block";
     }
 }
 
 function confirmmsg() {
-
     confirm("do you want to proceed?");
     play();
+}
+
+function startgame() {
+    startGame.style.display = "none";
+    gamePage.style.display = "block";
 
 }
+// function to generate random number between 1 and 14
